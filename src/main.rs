b50eb0912b9bffb974b3325d55375817ea579c92
T14X <khrystyna.tyvoniuk@gmail.com>
2026-01-05 14:06:14 +0100

    mutable var
    code is working, terminal shows a warning

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,18 @@
 
 
 //All vars by default are immutable in Rust
-
+/*
 fn main() {
     let x = 5;
     println!("The value of x is: {}", x);
     x=1;
     println!("The value of x is : {}", x);
+}
+*/
+//make it mutable: 
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    let x=1;
+    println!("The value of x is: {}", x);
 }
