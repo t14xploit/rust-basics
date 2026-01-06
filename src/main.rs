3bcf953cdbac4d272a13758a1adec463e981af96
T14X <khrystyna.tyvoniuk@gmail.com>
2026-01-06 19:08:42 +0100

    while, for loops, a.iter

--- src/main.rs
+++ src/main.rs
@@ -132,3 +132,28 @@ fn main(){
         break;
     }
 }
+
+//while loop 
+fn main() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+//for loop
+
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+}
+
+//the a.iter we turn a into an iterable
